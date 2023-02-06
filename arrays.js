1b56355581fad97bb3f6f6404f62d768b7e3efe6
Karen Buitrago <karenbuitrago2519@gmail.com>
2023-02-06 17:30:12 -0500

    Arrays y algunos metodos

---
+++ arrays.js
@@ -0,0 +1,64 @@
+var listaDeCompras =[];
+
+//Agregar elementos
+listaDeCompras[1]= "Lechuga";
+listaDeCompras[3]= "Tomates";
+console.log(listaDeCompras);
+
+//Acceder elementos
+var elementoDelArray=listaDeCompras[1];
+console.log(elementoDelArray);
+
+//Metodo length
+console.log(listaDeCompras.length);
+
+//Metodo push -> añadir al final
+var colores =["amarillo", "azul"];
+colores.push("rojo");
+console.log(colores)
+
+//Metodo unshift -> añadir al principio
+colores.unshift("verde");
+console.log(colores);
+
+//Metodo pop -> eliminar ultimo dato
+colores.pop();
+console.log(colores);
+
+//Metodo shift -> eliminar el primer dato
+colores.shift();
+console.log(colores);
+
+//Metodo includes
+var pintores= ["Picazzo", "Velazquez", "Van gogh", "Dali"];
+var incluyeDali = pintores.includes("Dali");
+console.log(incluyeDali);
+
+//Metodo every
+var numero =[7, 6, 8, 9];
+var cumplenCondicion = numero.every((num) => {
+    return num>5;
+});
+
+//Metodo split
+var palabra = "Henri";
+var palabraSeparada = palabra.split("");
+palabraSeparada.pop();
+palabraSeparada.push("y");
+var palabraCorrecta= palabraSeparada.join("");
+console.log(palabraCorrecta);
+
+//Metodo forEach
+var datos = [1, 2, 3, 4];
+datos.forEach((num)=>console.log(num));
+
+datos.forEach((num)=>{
+if(num ===3){
+    console.log(num)
+    }
+});
+
+//Metodo map 
+var sumaMasUno = datos.map(num =>{return num + 1});
+console.log(sumaMasUno);
+
