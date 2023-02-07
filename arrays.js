3493e9c10641f63c5c1014bf428ec8326c692e29
Karen Buitrago <karenbuitrago2519@gmail.com>
2023-02-06 21:51:43 -0500

    Array/ Metodos/ Bucles

--- arrays.js
+++ arrays.js
@@ -62,3 +62,28 @@ if(num ===3){
 var sumaMasUno = datos.map(num =>{return num + 1});
 console.log(sumaMasUno);
 
+//Bucles for con array
+var arr = [1, 2, 3, 4, 5];
+for(var i = 0; i < arr.length; i++){
+    console.log([i]);
+}
+
+//Con funciones
+function encontrarLetra(string){
+    var letras = string.split("");
+    for(var i =0; i < letras.length; i++){
+       if (letras[i]==="p"){
+        console.log("Si existe la letra P");
+        letras.join("");
+       }
+    }
+}
+encontrarLetra("JavaScript");
+
+//Bucle while con array
+var arrUno =[];
+while(arrUno.length<5){
+    arrUno.push("BOOM");
+}
+
+console.log(arrUno);
